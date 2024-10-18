b08bdd09b2c50d41906319f1d2c36fae6807abc0
Nikos Mastoris <nmastoris@admin.grnet.gr>
2024-10-18 09:55:28 +0000

    Fix typo and fix some tests regarding error handling

--- tests/test_server_24_oauth2_authorization_endpoint.py
+++ tests/test_server_24_oauth2_authorization_endpoint.py
@@ -347,7 +347,7 @@ class TestEndpoint(object):
         _context = self.endpoint.upstream_get("context")
         request = {"redirect_uri": "https://rp.example.com/cb"}
 
-        with pytest.raises(KeyError):
+        with pytest.raises(UnknownClient):
             verify_uri(_context, request, "redirect_uri", "client_id")
 
     def test_verify_uri_unregistered(self):
@@ -552,7 +552,7 @@ class TestEndpoint(object):
         _context.cdb["client_id"] = {"redirect_uris": [("https://rp.example.com/cb", {})]}
 
         request = {"redirect_uri": "https://rp.example.com/cb?foo=bob"}
-        with pytest.raises(ValueError):
+        with pytest.raises(URIError):
             verify_uri(_context, request, "post_logout_redirect_uri", "client_id")
 
     def test_verify_uri_none_registered(self):
